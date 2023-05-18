19b481036a4ea7f6da87824ea1ebd285a63d0edf
Sara G.A <s.garrofe@gmail.com>
2023-05-18 16:57:28 +0200

    Creación de ficheros posts.js y autores.js
    Distribución de rutas según las peticiones y primera puesta en práctica de las urls más sencillas.

--- appBlog/app.js
+++ appBlog/app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const autoresRouter = require('./routes/autores');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+app.use('/autores', autoresRouter);
 
 module.exports = app;
---
+++ appBlog/urls.md
@@ -0,0 +1,19 @@
+## obtención y creación de los posts y de los autores
+
+### POSTS
+
+- GET /posts/all 
+- GET /posts/postId (Cada post recuperar deberia ir acompañadao de todos los datos del autor de este )
+- POST /posts/new
+- DELETE /posts/delete/postId
+- PUT /posts/edit
+
+### AUTORES
+
+- GET /autores/all 
+- GET /autores/autorId 
+- POST /autores/new
+- DELETE /autores/delete/autorId
+- PUT /autores/edit
+
+# se debe definir una url que nos permita recuperar los diferentes posts escritos por un autor en concreto 
